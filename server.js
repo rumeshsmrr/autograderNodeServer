ae18c89ce3e741bacad5d2b2a2454a1ecf0585df
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-05-28 08:22:59 +0530

    Comment out credentials option in CORS configuration for clarity

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(
     origin: ["http://localhost:3000", "https://autograder-client.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    // credentials: true,
   })
 );
 
